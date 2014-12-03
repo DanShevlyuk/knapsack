4b08d66b4a8645156db5e58870903721407c3fe2
Dan Shevlyuk <twistedplane@gmail.com>
2014-12-04 00:05:57 +0300

    Initial commit

---
+++ knapsack/knapsack.cpp
@@ -0,0 +1,10 @@
+//
+//  knapsack.cpp
+//  knapsack
+//
+//  Created by Dan Shevlyuk on 12/4/14.
+//  Copyright (c) 2014 Dan Shevlyuk. All rights reserved.
+//
+
+#include "knapsack.h"
+#include <stdio.h>
---
+++ knapsack/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  knapsack
+//
+//  Created by Dan Shevlyuk on 12/4/14.
+//  Copyright (c) 2014 Dan Shevlyuk. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
