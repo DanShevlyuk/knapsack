faa55f64743a004d633268266f2ed4a002bbc6fc
Dan Shevlyuk <twistedplane@gmail.com>
2014-12-05 21:11:29 +0300

    Make dynamic_knapsack

--- knapsack/knapsack.cpp
+++ knapsack/knapsack.cpp
@@ -1,10 +1,99 @@
 //
-//  knapsack.cpp
-//  knapsack
+// knapsack.cpp
 //
-//  Created by Dan Shevlyuk on 12/4/14.
-//  Copyright (c) 2014 Dan Shevlyuk. All rights reserved.
+// Created by Daniil Shevlyuk on December 4, 2014
+//
 //
 
 #include "knapsack.h"
 #include <stdio.h>
+
+
+//============== dynamic knapsack =================
+
+#pragma mark - dynamic knapsack
+
+void print_matrix(int **matrix, int n, int k) {
+    for (int i = 0; i < n; i ++) {
+        for (int j = 0; j < k; j++) {
+            cout << matrix[i][j] << "\t";
+        }
+        cout << '\n';
+    }
+    
+}
+
+int knapsack::dynamic_knapsack(item* items, int n, int capacity) {
+    this->items = items;
+    this->n = n;
+    
+    int** matrix = make_matrix_for_dynamic(n, capacity);
+    
+    int res = dynamic_recursive_call(n, capacity, matrix);
+    
+    print_matrix(matrix, n + 1, capacity + 1);
+    
+    delete [] matrix;
+    
+    return res;
+}
+
+
+int knapsack::dynamic_recursive_call(int items_to_pack, int capacity, int** matrix) {
+
+    if (matrix[items_to_pack][capacity] == -1) {
+        if (items[items_to_pack - 1].weight <= capacity) {
+            
+            int a = dynamic_recursive_call(items_to_pack - 1, capacity, matrix);
+            int b = dynamic_recursive_call(items_to_pack - 1,
+                                           (capacity - items[items_to_pack - 1].weight), matrix) + items[items_to_pack - 1].cost;
+            
+            matrix[items_to_pack][capacity] = std::max(a, b);
+        } else {
+            matrix[items_to_pack][capacity] = dynamic_recursive_call(items_to_pack - 1, capacity, matrix);
+        }
+    }
+    
+    return matrix[items_to_pack][capacity];
+}
+
+int** knapsack::make_matrix_for_dynamic(int n, int capacity) {
+    int** matrix = new int*[n + 1];
+    for(int i = 0; i < n + 1; i++) {
+        matrix[i] = new int[capacity + 1];
+        
+        for (int j = 0; j < capacity + 1; j ++) {
+            if (i == 0) {
+                matrix[i][j] = 0;
+            } else {
+                if (j == 0) {
+                    matrix[i][j] = 0;
+                } else {
+                    matrix[i][j] = -1;
+                }
+            }
+
+        }
+    }
+    
+    return matrix;
+}
+
+
+//============== end of backtracking knapsack =================
+
+
+
+//============== backtracking knapsack =================
+
+//#pragma mark - backtracking knapsack
+//
+//virtual int backtrack_knapsack(item* items, int n, int capacity) {
+//    
+//}
+//
+//virtual int backtracking_recursive_call(int current_item, int current_weight, int current_cost, int capacity) {
+//    
+//}
+
+//============== end of backtracking knapsack =================
--- knapsack/main.cpp
+++ knapsack/main.cpp
@@ -7,9 +7,25 @@
 //
 
 #include <iostream>
+#include "knapsack.h"
+
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    
+    item* items = new item[6];
+    items[0] = item(55, 3);
+    items[1] = item(80, 2);
+    items[2] = item(60, 4);
+    items[3] = item(45, 1);
+    items[4] = item(105, 3);
+    items[5] = item(50, 1);
+    
+    knapsack ks;
+    
+    
+    cout << ks.dynamic_knapsack(items, 6, 10);
+    
+    
+    
     return 0;
 }
